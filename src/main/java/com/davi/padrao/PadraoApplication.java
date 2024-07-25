7f6ff18b28e4d504bfa0982b678940d0c186af29
DaviCarvalhoo <davicarvalhotech@gmail.com>
2024-07-25 19:07:02 -0300

    Initial commit

---
+++ src/main/java/com/davi/padrao/PadraoApplication.java
@@ -0,0 +1,13 @@
+package com.davi.padrao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadraoApplication.class, args);
+	}
+
+}
